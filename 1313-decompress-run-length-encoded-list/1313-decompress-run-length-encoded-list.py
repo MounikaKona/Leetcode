0ee80ca75432138d0788206ef973d0698a5c3c87
Latha Kona <131299704+MounikaKona@users.noreply.github.com>
2024-05-22 21:20:08 -0400

    Time: 35 ms (80.81%), Space: 12.1 MB (49.83%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
@@ -3,10 +3,7 @@ class Solution(object):
         output = []
         for i in range(0, len(nums),2):
             freq = nums[i]
-            freq_list = []
-            for j in range(nums[i]+1):
-                freq_list.append(nums[i])
-            for k in range(1, len(freq_list)):
-                output.append(nums[i+1])
+            value = nums[i+1]
+            output.extend([value] * freq)
         return output
         
